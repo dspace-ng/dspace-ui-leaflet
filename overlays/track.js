85d43d296e53c3292bc1e79111b9e29813750fe2
elf Pavlik <perpetual-tripper@wwelves.org>
2013-10-15 20:16:25 +0200

    adjusted trackOverlay to new schema of positions stored in Track

--- overlays/track.js
+++ overlays/track.js
@@ -2,8 +2,8 @@ var TrackOverlay = Backbone.View.extend({
 
   initialize: function() {
     this.layer = this.options.layer;
-    _.bindAll(this, 'add');
-    this.collection.on('add', this.add);
+    _.bindAll(this, '_newPosition');
+    this.collection.on('add', this._newPosition);
 
     this.track = new L.Polyline([], {
       color: this.options.color
@@ -16,8 +16,22 @@ var TrackOverlay = Backbone.View.extend({
     }
   },
 
-  add: function(location) {
-    this.track.addLatLng(location.toJSON());
+  _newPosition: function(location) {
+    var coords = location.toJSON().coords;
+    var point = { lat: coords.latitude, lon: coords.longitude };
+    var points = this.track.getLatLngs();
+    if(points.length === 0){
+      this._addPoint(point);
+      return;
+    }
+    var last = points[points.length - 1];
+    if(coords.latitude != last.lat || coords.longitude != last.lng){
+      this._addPoint(point);
+    }
+  },
+
+  _addPoint: function(latLng) {
+    this.track.addLatLng(latLng);
   }
 });
 
